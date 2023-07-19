c79744ef407eb596b0e0fefc2e496c83ed851b06
bsstayo <k99hoon@gmail.com>
2023-07-19 21:47:11 +0900

    After Chapter 6, useEffect, double-check

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({text}){
-    return <button className={styles.btn}>{text}</button>;
+function Button({text, fn}){
+    return <button className={styles.btn} onClick={fn}>{text}</button>;
 }
 
 Button.propTypes = {
